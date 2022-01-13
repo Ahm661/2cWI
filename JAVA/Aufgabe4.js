64e46534491dd99c9cf8d089212c4462a9af14e8
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-01-13 14:26:56 +0100

    Java Script Aufgabe4 Fertig

---
+++ JAVA/Aufgabe4.js
@@ -0,0 +1,34 @@
+// Zähle von 7 bis 45
+// Zähle von 9 bis 99
+// Zähle rückwärts von 10 bis 0
+// Zähle rückwärts von 1234 bis 1207
+
+// Gib Deinen Namen genau 8 mal aus
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let count = 7 ;count <=45; count ++ ){
+    console.log(count  )
+}
+console.log( "Aufgabe2")
+for (let count = 9 ;count <=99; count ++ ){
+    console.log(count  )
+}
+console.log("Aufgabe3")
+for (let count = 10 ;count >=0; count -- ){
+    console.log(count  )
+}
+console.log("Aufgabe4")
+for (let count = 1234 ;count >=1207; count -- ){
+    console.log(count  )
+}
+console.log("Aufgabe5")
+for (let count = 0 ;count <=10; count ++ ){
+    console.log("Ahmet")
+}
+console.log("Aufgabe6")
+for (let count = 0 ;count <=10; count ++ ){
+    console.log(count  )
+}
+console.log("Ahmet")
+for (let count = 10 ;count >=0; count -- ){
+    console.log(count  )
+}
