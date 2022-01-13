6c43c0f8dfcbbc8f1739418790fddc999cfe56bd
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-01-13 09:07:03 +0100

    java anfang

---
+++ JAVA/Script.js
@@ -0,0 +1,12 @@
+console.log("Hallo" );
+console.log("Tolle Sache");
+
+let a = 7;
+let b = 8;
+let c = 10;
+let result1 = a+b+c;
+console.log ("A+B+C=" + result1);
+result1 = (a+b)/c;
+ console.log("(A+B)/C=" + result1)
+ result1 = c-a+b;
+ console.log("C-A+B=" + result1) 
