fea24bc8fa99afe13571aa464b7b8610aa8ebf9b
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-01-13 13:42:43 +0100

    Javascript Aufgabe3 Fertig

---
+++ JAVA/Aufgabe3.js
@@ -0,0 +1,14 @@
+let banane= "Banane";
+let apple= "Apple";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let bananegewicht = 0.34;
+let applegewicht = 0.22;
+
+let result=8*applegewicht*applePricePerKilo;
+console.log(result)
+
+let result1 = 17*bananegewicht*bananaPricePerKilo;
+console.log(result1);
