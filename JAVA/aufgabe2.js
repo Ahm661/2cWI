1e6d30859eab3217a6f23b87adeb0bc32c8b349c
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-01-13 13:26:55 +0100

    Java script Aufgabe 2 Fertig gemacht

---
+++ JAVA/aufgabe2.js
@@ -0,0 +1,10 @@
+let a=17.4;
+let b=3.9;
+let result=a/b;
+
+console.log(" The result of " + a + " divided by " + b + " is " + result );
+
+let vorname="Ahmet";
+let Nachname="Akbulut";
+
+console.log(" Ich heise " + vorname +" " + Nachname );
