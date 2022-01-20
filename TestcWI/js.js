80e9e36eeeba677aaf9143865f16de0e0deb7285
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-01-20 14:38:45 +0100

    test JS

---
+++ TestcWI/js.js
@@ -0,0 +1,8 @@
+for (let count = 0 ;count <=8; count ++ ){
+    console.log(count  )
+}
+console.log("Das Wars!")
+let a=-2;
+for (let count = 11 ;count >=-3; count -- ){
+    console.log(count  )
+}
