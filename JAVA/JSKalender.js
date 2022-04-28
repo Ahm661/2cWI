c3bab65197cd32ef20ad35555f725b23dfdbbcac
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-04-28 20:09:42 +0200

    aufgabe 12 weitergemacht und kalender erste hälfte gemacht

---
+++ JAVA/JSKalender.js
@@ -0,0 +1,35 @@
+printMonth(31);
+printMonth(28);
+printMonth(31);
+printMonth(30);
+printMonth(31);
+printMonth(30);
+printMonth(31);
+printMonth(31);
+printMonth(30);
+printMonth(31);
+printMonth(30);
+printMonth(31);d3qa
+
+function printMonth(amountOFdays){
+    days = "";
+    for (let index = 1; index <= amountOFdays; index++) {
+        if (index<10) {
+            days = days + "0" + index + " | "
+        }
+        else{
+            days = days + index + " | "
+        }
+      
+        
+        if (index%7==0){
+            console.log(days);
+            days="";
+        }
+        
+    }
+    
+    console.log(days);
+}
+
+
